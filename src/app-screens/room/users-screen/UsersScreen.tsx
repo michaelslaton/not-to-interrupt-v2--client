195066bf72ff8b83120df4fd5aef599eab1e1c29
RenShadecrest <michaelaaronphoto@gmail.com>
2025-11-06 17:04:04 -0800

    continued flushing out new user controllers, app seperates self user from others, seperates buttons accessible by user, user is seperated and placed first

--- src/app-screens/room/users-screen/UsersScreen.tsx
+++ src/app-screens/room/users-screen/UsersScreen.tsx
@@ -1,26 +1,59 @@
 import type { JSX } from 'react';
 import { useAppContext } from '../../../App';
 import type { UserType } from '../../../types/UserType.type';
+import { faHandPaper } from '@fortawesome/fontawesome-free-solid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import type { IconProp } from '@fortawesome/fontawesome-svg-core';
 import './usersScreen.css';
 
 const UsersScreen = () => {
   const { appState } = useAppContext();
   const userList: UserType[] = appState.roomData!.users;
-  console.log(userList[0].controller)
+  
+  const isOwner = (id: string, ownerId: string) => {
+    if(id === ownerId) return true;
+    return false;
+  };
 
   const populateUsers = (): JSX.Element[] => {
-    const formattedUsers = userList.map((user,i)=>(
-      <div key={i} className='userController'>
+    const ownerUser = (
+      <div key='owner' className='userController owner'>
+        <div className='userName'>
+          {appState.user!.name}
+        </div>
         <div>
           🎙️
         </div>
-        <div>
-          {user.name}
-        </div>
+        <button className='controllerButton owner'>
+          <FontAwesomeIcon icon={faHandPaper as IconProp}/>
+        </button>
+        <button className='controllerButton owner'>
+          AFK
+        </button>
       </div>
-    ));
+    );
 
-    return formattedUsers;
+    const otherUsers = userList
+      .filter(user => !isOwner(user.id, appState.user!.id))
+      .map((user, i) => (
+        <div key={i} className='userController'>
+          <div className='userName'>
+            {user.name}
+          </div>
+          <button className={`controllerButton hasMic ${user.controller.hasMic && 'active'}`}>
+            🎙️
+          </button>
+          <div className={`controllerButton handUp ${user.controller.handUp && 'active'}`}>
+            <FontAwesomeIcon icon={faHandPaper as IconProp}/>
+          </div>
+          <div className={`controllerButton afk ${user.controller.afk && 'active'}`}>
+            AFK
+          </div>
+        </div>
+      )
+    );
+
+    return [ownerUser, ...otherUsers];
   };
 
   return (
